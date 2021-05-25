7176bbf46c555d9f0980abef60a4ecb19cdcd2b4
Tennessine_699 <d.hirano123@gmail.com>
2021-05-25 21:28:50 +0900

    added eat button changed eatall button

--- works/counter.js
+++ works/counter.js
@@ -4,6 +4,13 @@ const countUp = () => {
   const countElement = document.querySelector('#count')
   countElement.innerText = count + ' クッキー'
 }
+const countDown = () => {
+  if (count > 0) {
+    count--
+  }
+  const countElement = document.querySelector('#count')
+  countElement.innerText = count + ' クッキー'
+}
 const countReset = () => {
   count=0
   const countElement = document.querySelector('#count')
